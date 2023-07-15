56de9662cae157560630835fd90df9b6dbc1d366
dylan.owen <dylan.owen@workday.com>
2023-07-15 17:48:55 +0200

    allow Deref enum variants
    Based on [1], by original author Dylan Owen.
    
    I made the code compatible with the latest version of `syn` (tested with
    2.0.26) and implemented one of the suggestion from the earlier review of
    the original change [2] (the one about having an explicit
    EnumDispatchVariantAttribute).
    
    [1] https://gitlab.com/dylanowen/enum_dispatch/-/commit/9d3a4578d3f288689f5a8925de29f2f9ee97e382
    
    Co-authored-by: Sebastiano Barrera <sebastiano.barrera@gmail.com>

--- src/enum_dispatch_variant.rs
+++ src/enum_dispatch_variant.rs
@@ -7,13 +7,18 @@
 
 use std::iter::FromIterator;
 
-use quote::TokenStreamExt;
+use quote::{ToTokens, TokenStreamExt};
+use syn::spanned::Spanned;
 
 use crate::filter_attrs::FilterAttrs;
 
-/// A structure that can be used to store syntax information about an `enum_dispatch` enum variant.
+pub const ENUM_DISPATCH: &str = "enum_dispatch";
+pub const DEREF_ATTRIBUTE: &str = "deref";
+
+/// A structure that can be used to store syntax nformation about an `enum_dispatch` enum variant.
 #[derive(Clone)]
 pub struct EnumDispatchVariant {
+    pub deref: bool,
     pub attrs: Vec<syn::Attribute>,
     pub ident: syn::Ident,
     pub field_attrs: Vec<syn::Attribute>,
@@ -23,7 +28,23 @@ pub struct EnumDispatchVariant {
 /// Allows `EnumDispatchItem`s to be parsed from `String`s or `TokenStream`s.
 impl syn::parse::Parse for EnumDispatchVariant {
     fn parse(input: syn::parse::ParseStream) -> syn::parse::Result<Self> {
-        let attrs = input.call(syn::Attribute::parse_outer)?;
+        let mut deref = false;
+        let mut extra_attrs = Vec::new();
+
+        for attr in input.call(syn::Attribute::parse_outer)?.into_iter() {
+            match parse_variant_attribute(&attr)? {
+                Some(EnumDispatchVariantAttribute::Deref) => {
+                    deref = true;
+                    // The deref attribute needs to be removed, to avoid it leaking into the
+                    // expanded code (where the compiler wouldn't know how to handle it).
+                }
+                None => {
+                    // An attribute handled by some other library
+                    extra_attrs.push(attr);
+                }
+            }
+        }
+
         let ident: syn::Ident = input.parse()?;
         let (field_attrs, ty) = if input.peek(syn::token::Brace) {
             unimplemented!("enum_dispatch variants cannot have braces for arguments");
@@ -41,7 +62,8 @@ impl syn::parse::Parse for EnumDispatchVariant {
             (vec![], into_type(ident.clone()))
         };
         Ok(EnumDispatchVariant {
-            attrs,
+            deref,
+            attrs: extra_attrs,
             ident,
             field_attrs,
             ty,
@@ -79,4 +101,29 @@ fn into_type(ident: syn::Ident) -> syn::Type {
         },
         qself: None,
     })
+}
+
+enum EnumDispatchVariantAttribute {
+    Deref,
+}
+
+fn parse_variant_attribute(
+    attr: &syn::Attribute,
+) -> syn::parse::Result<Option<EnumDispatchVariantAttribute>> {
+    if attr.meta.path().is_ident(ENUM_DISPATCH) {
+        let ident = attr.parse_args::<syn::Ident>()?.to_string();
+        match ident.as_str() {
+            DEREF_ATTRIBUTE => Ok(Some(EnumDispatchVariantAttribute::Deref)),
+            _ => {
+                let message = format!(
+                    "Expected #[enum_dispatch(...)] but found {}",
+                    attr.to_token_stream()
+                );
+                Err(syn::parse::Error::new(attr.span(), message))
+            }
+        }
+    } else {
+        // It's an attribute that we don't handle.
+        Ok(None)
+    }
 }
